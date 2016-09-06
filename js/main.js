4c7b9001044b43a8aac357fba5a66938a7cba597
LucienPortfolio <lucien541368746@gmail.com>
2016-09-06 18:50:14 -0400

    1

--- js/main.js
+++ js/main.js
@@ -1,41 +1,47 @@
+var list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+var a = [];
+var pos = [];
+var v = [];
+
 document.addEventListener('DOMContentLoaded',function(){
-    var posX = [];
-    var posY = [];
-    var spdX = [];
-    var spdY = [];
-    var rotY = [];
-    var size = [];
-    for(var i = 0; i<7;i++){
-        posX.push(Math.random()*0.6);
-        posY.push(Math.random()*0.44);
-        spdX.push(Math.random()*0.05);
-        spdY.push(Math.random()*0.05);
-        size.push(Math.random()*80+80);
-        document.getElementsByClassName('letter')[i].style.marginLeft = posX[i]*100+'%';
-        document.getElementsByClassName('letter')[i].style.marginTop = posY[i]*100+'%';
-        rotY.push(15+Math.random()*15);
-        document.getElementsByClassName('letter')[i].style.transform = 'rotateY('+rotY[i]+'deg)';
-        document.getElementsByClassName('letter')[i].style.WebkitTransform = 'rotateY('+rotY[i]+'deg)';
-        document.getElementsByClassName('letter')[i].style.fontSize = size[i]+'px';
+    for(var i = 0; i<60; i++){
+        var big = document.getElementById('background-animation');
+        var newDiv = document.createElement('div');
+        newDiv.innerHTML = list[parseInt(Math.random()*25)];
+        newDiv.style.marginLeft = Math.random()*100+'%';
+        newDiv.style.display = 'inline';
+        newDiv.style.position = 'fixed';
+        newDiv.style.marginTop = '-20%';
+        newDiv.style.fontSize = Math.random()*120+40+'px'
+        pos.push(-20-Math.random()*30);
+        v.push(Math.random()*0.1+0.05);
+        newDiv.className = 'letter';
+        big.appendChild(newDiv)
     }
 
-    function timer(ms, i){
+    function timer(){
         setInterval(function(){
-                spdX[i]=(0.05-Math.random()*0.1);
-                spdY[i]=(0.05-Math.random()*0.1);
-                console.log(document.getElementsByClassName('letter')[i].style.transform);
-                document.getElementsByClassName('letter')[i].style.marginLeft = (posX[i]+spdX[i])*100+'%';
-                document.getElementsByClassName('letter')[i].style.marginTop = (posY[i]+spdY[i])*100+'%';
-                rotY[i]=-rotY[i];
-                document.getElementsByClassName('letter')[i].style.transform = 'rotateY('+rotY[i]+'deg)';
-                document.getElementsByClassName('letter')[i].style.WebkitTransform = 'rotateY('+rotY[i]+'deg)';
-        },ms)
+            for(var i = 0; i<200; i++){
+                var letters = document.getElementsByClassName('letter')[i];
+                pos[i] = pos[i]+=v[i];
+                letters.style.marginTop = pos[i]+'%';
+                if(pos[i]>100){
+                    letters.innerHTML = list[parseInt(Math.random()*25)];
+                    letters.style.fontSize = Math.random()*120+40+'px'
+                    pos[i] = -10;
+                    letters.style.marginLeft = Math.random()*100+'%';
+                }
+            }
+        },10);
     }
 
-    for(var i = 0; i <7; i++){
-        timer(Math.random()*2000+3000, i);
-    }
-})
+    timer();
+});
+/**
+ * Created by Lucien on 9/6/2016.
+ */
+
 /**
  * Created by Lucien on 8/28/2016.
  */
